70508e40428d74c32931ca2e31f1b78d783db544
Hércules Almeida <herculesalmeida021@gmail.com>
2025-02-16 13:23:54 -0300

    Add files via upload

---
+++ Ex 003.py
@@ -0,0 +1,4 @@
+D = input ('Em qual dia você nasceu?')
+M = input ('Qual mês?')
+A = input ('Qual o ano?')
+print ('De acordo com as informações disponibilizadas você nasceu no dia',D,'de',M,'de',A,'correto?')
---
+++ Ex 004.py
@@ -0,0 +1,4 @@
+n1 = input ('Informe o primiero número')
+n2 = input ('Informe o segundo número')
+s = int(n1)+int(n2) 
+print ('A soma de',n1,'+',n2,'é igual a',s,'!')
